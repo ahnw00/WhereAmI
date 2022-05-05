a5c3d69cb51b1212a98edf2f009a3ed0f5653234
sangsangsang369 <parkse0324@gmail.com>
2022-05-05 23:06:34 +0900

    asdf

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -10,12 +10,11 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public InputField NickNameInput;
     public GameObject DisconnectPanel, ConnectPanel;
     
-    //public GameObject RespawnPanel;
+    //플레이어 리스팅 관련 선언
     [SerializeField] Transform playerListParent;
     [SerializeField] GameObject playerListing;
     private Dictionary<int, GameObject> playerListEntries = new Dictionary<int, GameObject>();
     bool isPlayerReady = false;
-    
 
     void Awake()
     {
@@ -37,31 +36,24 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         DisconnectPanel.SetActive(false);
         Spawn();
         ConnectPanel.SetActive(true);
-        //player list
-        /*Photon.Realtime.Player[] players = PhotonNetwork.PlayerList;
-        for (int i = 0; i < players.Length; i++)
-        {
-            Instantiate(playerListing, playerListParent).GetComponent<PlayerListing>().SetPlayerInfo(players[i]);
-            //내가 방에 들어가면 방에있는 사람 목록 만큼 이름표 뜨게 하기
-        }*/
-
         
+        //플레이어 리스트
         if (playerListEntries == null)
 		{
 			playerListEntries = new Dictionary<int, GameObject>();
 		}
 
         foreach (Photon.Realtime.Player p in PhotonNetwork.PlayerList)
-		{
+		{   //플레이어 리스트 내용물 생성
 			GameObject entry = Instantiate(playerListing, playerListParent);
 			entry.GetComponent<PlayerListing>().SetPlayerInfo(p.ActorNumber, p.NickName);
-
+            //플레이어 준비 상태 
 			object isPlayerReady;
 			if (p.CustomProperties.TryGetValue("isPlayerReady", out isPlayerReady))
 			{
 				entry.GetComponent<PlayerListing>().SetPlayerReady((bool)isPlayerReady);
 			}
-
+            //입장한 플레이어 리스트에 추가
 			playerListEntries.Add(p.ActorNumber, entry);
 		}
     }
@@ -71,18 +63,18 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.Instantiate("PlayerObject", new Vector3(Random.Range(60, 96), 80f, Random.Range(-3, 32)), Quaternion.identity);
     }
 
+    //전체 플레이어 준비 완료 됐는지 체크
     private bool CheckPlayersReady()
 	{
 		if (!PhotonNetwork.IsMasterClient)
 		{
 			return false;
 		}
-
+        //각 플레이어들의 isPlayerReady가 참인지 체크
 		foreach (Photon.Realtime.Player p in PhotonNetwork.PlayerList)
 		{
 			object isPlayerReady;
-
-			if (p.CustomProperties.TryGetValue("isPlayerReady", out isPlayerReady))
+            if (p.CustomProperties.TryGetValue("isPlayerReady", out isPlayerReady))
 			{
 				if (!(bool)isPlayerReady)
 				{
@@ -94,11 +86,10 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 				return false;
 			}
 		}
-
 		return true;
 	}
 
-    //새 플레이어 들어오면 리스트 끝에 생성
+    //새 플레이어 들어올 때 호출되는 함수
     public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
     {
         GameObject entry = Instantiate(playerListing, playerListParent);
@@ -107,6 +98,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         playerListEntries.Add(newPlayer.ActorNumber, entry);
     }
 
+    //customProperty에 변화 있을 때 호출되는 함수
     public override void OnPlayerPropertiesUpdate(Photon.Realtime.Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
     {
         if (playerListEntries == null)
@@ -119,12 +111,13 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 		{
 			object isPlayerReady;
 			if (changedProps.TryGetValue("isPlayerReady", out isPlayerReady))
-			{
+			{   //SetPlayerReady 함수 불러서 준비중/준비완료 표기 바꾸기
 				entry.GetComponent<PlayerListing>().SetPlayerReady((bool)isPlayerReady);
 			}
 		}
-
     }
+
+    //플레이어가 방 떠날 때 호출되는 함수
     public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
 	{
 		Destroy(playerListEntries[otherPlayer.ActorNumber].gameObject);
