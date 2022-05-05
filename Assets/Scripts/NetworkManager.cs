b28098b021df207c8c6ffd955af86cb13617993c
sangsangsang369 <parkse0324@gmail.com>
2022-05-05 22:38:05 +0900

    playerList

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -8,8 +8,14 @@ using UnityEngine.UI;
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     public InputField NickNameInput;
-    public GameObject DisconnectPanel;
+    public GameObject DisconnectPanel, ConnectPanel;
+    
     //public GameObject RespawnPanel;
+    [SerializeField] Transform playerListParent;
+    [SerializeField] GameObject playerListing;
+    private Dictionary<int, GameObject> playerListEntries = new Dictionary<int, GameObject>();
+    bool isPlayerReady = false;
+    
 
     void Awake()
     {
@@ -30,6 +36,34 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         DisconnectPanel.SetActive(false);
         Spawn();
+        ConnectPanel.SetActive(true);
+        //player list
+        /*Photon.Realtime.Player[] players = PhotonNetwork.PlayerList;
+        for (int i = 0; i < players.Length; i++)
+        {
+            Instantiate(playerListing, playerListParent).GetComponent<PlayerListing>().SetPlayerInfo(players[i]);
+            //내가 방에 들어가면 방에있는 사람 목록 만큼 이름표 뜨게 하기
+        }*/
+
+        
+        if (playerListEntries == null)
+		{
+			playerListEntries = new Dictionary<int, GameObject>();
+		}
+
+        foreach (Photon.Realtime.Player p in PhotonNetwork.PlayerList)
+		{
+			GameObject entry = Instantiate(playerListing, playerListParent);
+			entry.GetComponent<PlayerListing>().SetPlayerInfo(p.ActorNumber, p.NickName);
+
+			object isPlayerReady;
+			if (p.CustomProperties.TryGetValue("isPlayerReady", out isPlayerReady))
+			{
+				entry.GetComponent<PlayerListing>().SetPlayerReady((bool)isPlayerReady);
+			}
+
+			playerListEntries.Add(p.ActorNumber, entry);
+		}
     }
 
     public void Spawn()
@@ -37,17 +71,81 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.Instantiate("PlayerObject", new Vector3(Random.Range(60, 96), 80f, Random.Range(-3, 32)), Quaternion.identity);
     }
 
+    private bool CheckPlayersReady()
+	{
+		if (!PhotonNetwork.IsMasterClient)
+		{
+			return false;
+		}
+
+		foreach (Photon.Realtime.Player p in PhotonNetwork.PlayerList)
+		{
+			object isPlayerReady;
+
+			if (p.CustomProperties.TryGetValue("isPlayerReady", out isPlayerReady))
+			{
+				if (!(bool)isPlayerReady)
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+    //새 플레이어 들어오면 리스트 끝에 생성
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        GameObject entry = Instantiate(playerListing, playerListParent);
+		entry.GetComponent<PlayerListing>().SetPlayerInfo(newPlayer.ActorNumber, newPlayer.NickName);
+
+        playerListEntries.Add(newPlayer.ActorNumber, entry);
+    }
+
+    public override void OnPlayerPropertiesUpdate(Photon.Realtime.Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (playerListEntries == null)
+		{
+			playerListEntries = new Dictionary<int, GameObject>();
+		}
+
+		GameObject entry;
+		if (playerListEntries.TryGetValue(targetPlayer.ActorNumber, out entry))
+		{
+			object isPlayerReady;
+			if (changedProps.TryGetValue("isPlayerReady", out isPlayerReady))
+			{
+				entry.GetComponent<PlayerListing>().SetPlayerReady((bool)isPlayerReady);
+			}
+		}
+
+    }
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+	{
+		Destroy(playerListEntries[otherPlayer.ActorNumber].gameObject);
+		playerListEntries.Remove(otherPlayer.ActorNumber);
+	}
+
     void Update() 
     { 
         if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected) 
         {
             PhotonNetwork.Disconnect(); 
         }
+        if(CheckPlayersReady())
+        {
+            Debug.Log("게임시작");
+        }
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         DisconnectPanel.SetActive(true);
-        //RespawnPanel.SetActive(false);
+        ConnectPanel.SetActive(false);
     }
 }
---
+++ Assets/Scripts/PlayerListing.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class PlayerListing : MonoBehaviour
+{
+    public Text playerNameText;
+    public Text playerStatusText;
+    public int ownerId;
+    private bool isPlayerReady;
+    public Button PlayerReadyButton;
+
+    public void Start()
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber != ownerId)
+        {
+            PlayerReadyButton.gameObject.SetActive(false);
+        }
+        else
+        {
+			ExitGames.Client.Photon.Hashtable readyStatus = new ExitGames.Client.Photon.Hashtable(){ {"isPlayerReady", isPlayerReady}};
+            PhotonNetwork.LocalPlayer.SetCustomProperties(readyStatus);
+
+            PlayerReadyButton.onClick.AddListener(() =>
+            {
+                isPlayerReady = !isPlayerReady;
+                SetPlayerReady(isPlayerReady);
+
+                ExitGames.Client.Photon.Hashtable newStatus = new ExitGames.Client.Photon.Hashtable() { { "isPlayerReady", isPlayerReady } };
+                PhotonNetwork.LocalPlayer.SetCustomProperties(newStatus);
+            });
+        }
+    }
+
+    public void SetPlayerInfo(int playerId, string playerName)
+    {
+        //Player = player;
+        playerNameText.text = playerName;
+        ownerId = playerId;
+        playerStatusText.text = "준비 중";
+        playerStatusText.color = Color.red;
+    }
+
+    public void SetPlayerReady(bool playerReady)
+    {
+        playerStatusText.text = playerReady ? "준비 완료" : "준비 중";
+        playerStatusText.color = playerReady ? Color.green : Color.red;
+    }
+} 
